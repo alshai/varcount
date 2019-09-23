29eda35a3b0974bba5d59948fb2c99b58f310134
Taher Mun <tmun1@jhu.edu>
2019-09-23 10:14:14 -0400

    moved compare_vcfs to next_gt. still unfinished

--- next_gt.cpp
+++ next_gt.cpp
@@ -10,66 +10,67 @@ struct CmpVcfArgs {
 };
 
 struct VarGT {
-    VarGT(int32_t p,
-          std::string a, std::string r, 
-          int32_t g1, int32_t g2) : 
-        pos(p),
-        alt(a), ref(r), 
-        gt1(g1), gt2(g2) {}
-    int32_t pos = 0;
-    std::string alt = "";
-    std::string ref = "";
-    int32_t gt1;
-    int32_t gt2;
-    // the following WON'T be used in comparators!
-    bool rec_start = 0;
-
-    static std::vector<VarGT> from_bcf(bcf_hdr_t* hdr, bcf1_t* b) {
-        std::vector<VarGT> vs;
-        char* ref = b->d.allele[0];
-        int ngt, gt1, gt2;
-        /* TODO: extract genotype from the hdr */
-        int32_t* gts = hts_util::get_genotype(hdr, b, &ngt);
+    /* Assuming one allele per line /shrug/ */
+    VarGT(bcf_hdr_t* hdr, bcf1_t* r) {
+        int ngt;
+        rec = bcf_dup(r);
+        // extract genotype
+        int32_t* gts = hts_util::get_genotype(hdr, rec, &ngt);
+        if (ngt < 2) { fprintf(stderr, "error getting genotype!\n"); exit(1);}
         gt1 = bcf_gt_allele(gts[0]);
         gt2 = bcf_gt_allele(gts[1]);
-        if (ngt < 2) { fprintf(stderr, "error getting genotype!\n"); exit(1);}
-        for (uint32_t i = 1; i < b->n_allele; ++i) {
-            char* alt = b->d.allele[i];
-            if (alt[0] == '.') continue;
-            vs.push_back(VarGT(b->pos, alt, ref, gt1, gt2)); 
-        }
-        vs[0].rec_start = 1;
-        return vs;
+        free(gts);
     }
+    ~VarGT() {
+        bcf_destroy(rec);
+    }
+    static std::vector<VarGT> from_bcf(bcf_hdr_t* hdr, bcf1_t* rec) {
+        std::vector<VarGT> v;
+        v.push_back(VarGT(hdr, rec));
+        return v;
+    }
+    int32_t gt1;
+    int32_t gt2;
+    bcf1_t* rec;
+    // the following WON'T be used in comparators!
+    bool rec_start = 0;
 };
 
 static inline bool var_match(const VarGT& lv, const VarGT& rv) {
-    return lv.pos == rv.pos && lv.alt == rv.alt && lv.ref == rv.ref && lv.gt1 == rv.gt1 && lv.gt2 == rv.gt2;
+    bcf1_t* l = lv.rec;
+    bcf1_t* r = rv.rec;
+    return l->pos == r->pos \
+                   && !strcmp(l->d.allele[1] , r->d.allele[1]) \
+                   && !strcmp(l->d.allele[0] , r->d.allele[0]) \
+                   && lv.gt1 == rv.gt1 \
+                   && lv.gt2 == rv.gt2;
 }
 
 void compare_vcfs(CmpVcfArgs args) {
     vcfFile* fp1 = bcf_open(args.fname1.data(), "r");
     bcf_hdr_t* hdr1 = bcf_hdr_read(fp1);
-    bcf_hdr_set_samples(hdr1, args.sample1.data(), 0);
+    if (args.sample1 != "" && bcf_hdr_set_samples(hdr1, args.sample1.data(), 0)) return;
 
     hts_util::contig2map_map<VarGT> contig2vars(hts_util::bcf_to_map<VarGT>(fp1, hdr1));
 
     vcfFile* fp2 = bcf_open(args.fname2.data(), "r");
     bcf_hdr_t* hdr2 = bcf_hdr_read(fp2);
-    bcf_hdr_set_samples(hdr2, args.sample2.data(), 0); // no genotypes needed here
+    if (args.sample2 != "" && bcf_hdr_set_samples(hdr2, args.sample2.data(), 0)) return;
     bcf1_t* rec = bcf_init();
 
     vcfFile* out_fp = bcf_open("-", "w");
     bcf_hdr_t* out_hdr = bcf_hdr_dup(hdr2);
     // print the header of fp2
-    bcf_hdr_write(out_fp, out_hdr);
+    if (!bcf_hdr_write(out_fp, out_hdr)) {
+        fprintf(stderr, "error writing header\n"); exit(1);
+    }
 
     int32_t pid = -1;
     hts_util::pos2var_map<VarGT>* vmap = nullptr;
     // We're going to assume one variant per line for both VCF files for now.
     while (!bcf_read(fp2, hdr2, rec)) {
         bcf_unpack(rec, BCF_UN_STR || BCF_UN_FMT);
-        auto vars2 = VarGT::from_bcf(hdr2, rec);
+        auto v2 = VarGT(hdr2, rec);
         if (rec->rid != pid) {
             vmap = &(contig2vars[bcf_hdr_id2name(hdr2, rec->rid)]);
         }
@@ -77,21 +78,29 @@ void compare_vcfs(CmpVcfArgs args) {
         auto vars1_iter = vmap->find(rec->pos);
         bool match = 0;
         if (vars1_iter != vmap->end()) {
-            for (const auto& v2: vars2) {
-                for (const auto& v1: vars1_iter->second) {
-                    if (var_match(v1, v2)) {
-                        match = 1;
-                        break;
-                    }
-                }
-                if (match) { // write record here
-                    bcf_write(out_fp, out_hdr, rec);
+            for (const auto& v1: vars1_iter->second) {
+                if (var_match(v1, v2)) {
+                    match = 1;
                     break;
-                } 
-            } match = 0;
+                }
+            }
+            if (!match) { // write record here
+                if (!bcf_write(out_fp, out_hdr, v2.rec)) {
+                    fprintf(stderr, "error writing\n"); exit(1);
+                }
+            }
         }  // not found
         pid = rec->rid;
     }
+   
+    bcf_destroy(rec);
+    bcf_hdr_destroy(out_hdr);
+    bcf_hdr_destroy(hdr1);
+    bcf_hdr_destroy(hdr2);
+    bcf_close(out_fp);
+    bcf_close(fp1);
+    bcf_close(fp2);
+
 }
 
 void print_help() {
@@ -106,15 +115,15 @@ matches(int) diffs(int) misses(int)\n");
 int main(int argc, char** argv) {
     CmpVcfArgs args;
     static struct option long_options[] {
-        {"sample1", required_argument, 0, 'x'},
-        {"sample2", required_argument, 0, 'y'},
-        {"help", no_argument, 0, 'h'},
+        {"s1", required_argument, NULL, 2},
+        {"s2", required_argument, NULL, 3},
+        {"help", no_argument, NULL, 'h'},
         {0,0,0,0}
     };
 
     int ch;
     int argpos = 0;
-    while ( (ch = getopt_long(argc, argv, "-:x:y:h", long_options, NULL)) != -1 ) {
+    while ( (ch = getopt_long(argc, argv, "-:h", long_options, NULL)) != -1 ) {
         switch(ch) {
             case 0:
                 break;
@@ -125,12 +134,13 @@ int main(int argc, char** argv) {
                 ++argpos;
                 break;
             case 2:
+                args.sample1 = std::string(optarg);
                 break;
-            case 'x':
-                args.sample1 = std::string(optarg); 
+            case 3:
+                args.sample2 = std::string(optarg);
                 break;
-            case 'y':
-                args.sample2 = std::string(optarg); 
+            case 'h':
+                print_help();
                 break;
             case '?':
                 print_help();
@@ -146,12 +156,6 @@ int main(int argc, char** argv) {
     if (args.fname1 == "" || args.fname2 == "") {
         print_help();
         fprintf(stderr, "error: vcf files are mandatory arguments\n");
-        exit(1);
-    }
-
-    if (args.sample1 == "" || args.sample2 == "") {
-        print_help();
-        fprintf(stderr, "error: sample names are mandatory arguments\n");
         exit(1);
     }
 
