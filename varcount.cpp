b341275b1e62ee7c690c71d54325ee3933f5b87d
Taher Mun <tahermun@gmail.com>
2019-09-24 11:17:02 -0400

    fixed filtering options. counts now moved to FORMAT/AD to comply w VCF spec

--- varcount.cpp
+++ varcount.cpp
@@ -7,6 +7,8 @@
 #include <getopt.h>
 #include "hts_util.hpp"
 
+constexpr float ERROR_RATE = 0.01;
+
 struct VcntArgs {
     std::string vcf_fname = "";
     std::string sam_fname = "";
@@ -18,6 +20,7 @@ struct VcntArgs {
     int diploid = 0;
     int alt_default = 0;
     int min_ac = 0;
+    int gl = 0;
 };
 
 static inline std::tuple<std::string,std::string> truncate_str_pair(const std::string& s1, const std::string& s2) {
@@ -50,7 +53,10 @@ void varcount(const VcntArgs& args) {
 
     vcfFile* vcf_fp = bcf_open(args.vcf_fname.data(), "r");
     bcf_hdr_t* vcf_hdr = bcf_hdr_read(vcf_fp);
-    bcf_hdr_set_samples(vcf_hdr, NULL, 0); // no genotypes needed here
+    if (bcf_hdr_set_samples(vcf_hdr, NULL, 0)) {
+        fprintf(stderr, "error setting samples\n");
+        exit(1);
+    } 
 
     hts_util::contig2map_map<hts_util::Var> contig2vars(hts_util::bcf_to_map(vcf_fp, vcf_hdr));
 
@@ -71,7 +77,7 @@ void varcount(const VcntArgs& args) {
             std::vector<hts_util::Var> aln_vars(hts_util::Var::from_bam(aln));
             if (args.verbose) {
                 fprintf(stderr, "a %s ", bam_get_qname(aln));
-                hts_util::print_varlist(aln_vars, stderr);
+                // hts_util::print_varlist(aln_vars, stderr);
             }
             // can we vectorize or at least parallelize this? would it be worth it?
             // as of now, the performance bottleneck is the VCF reading
@@ -86,11 +92,11 @@ void varcount(const VcntArgs& args) {
                         for (const auto& av: aln_vars) {
                             x |= var_match(vv, av);
                         }
-                        x ? ++(v_cached->ac) : ++(v_cached->rc);
+                        x ? ++(v_cached->ad[1]) : ++(v_cached->ad[0]);
                     }
                     if (args.verbose) {
                         fprintf(stderr, "v %s ", bam_get_qname(aln));
-                        hts_util::print_varlist(found->second, stderr);
+                        // hts_util::print_varlist(found->second, stderr);
                     }
                 }
             }
@@ -115,14 +121,22 @@ void varcount(const VcntArgs& args) {
     bcf_hdr_remove(out_vcf_hdr, BCF_HL_GEN, "bcftools_viewCommand");
     bcf_hdr_remove(out_vcf_hdr, BCF_HL_STR, NULL);
     bcf_hdr_set_version(out_vcf_hdr, "VCFv4.3");
-    bcf_hdr_append(out_vcf_hdr, "##INFO=<ID=ALTCNT,Number=1,Type=Integer,Description=\"Count of reads covering alt allele\">");
-    bcf_hdr_append(out_vcf_hdr, "##INFO=<ID=REFCNT,Number=1,Type=Integer,Description=\"Count of reads covering ref allele\">");
-    bcf_hdr_append(out_vcf_hdr, "##FORMAT=<ID=GT,Number=1,Type=String,Description=\"Genotype\">");
-    bcf_hdr_write(out_vcf_fp, out_vcf_hdr);
+    // bcf_hdr_append(out_vcf_hdr, "##INFO=<ID=ALTCNT,Number=1,Type=Integer,Description=\"Count of reads covering alt allele\">");
+    // bcf_hdr_append(out_vcf_hdr, "##INFO=<ID=REFCNT,Number=1,Type=Integer,Description=\"Count of reads covering ref allele\">");
+    bcf_hdr_append(out_vcf_hdr, "##FORMAT=<ID=AD,Number=R,Type=Integer,Description=\"Allelic Depth\">");
+    if (args.gt)
+        bcf_hdr_append(out_vcf_hdr, "##FORMAT=<ID=GT,Number=1,Type=String,Description=\"Genotype\">");
+    if (args.gl) 
+        bcf_hdr_append(out_vcf_hdr, "##FORMAT=<ID=PL,Number=G,Type=Integer,Description=\"List of Phred-scaled genotype likelihoods\">");
+    if (bcf_hdr_write(out_vcf_fp, out_vcf_hdr)) {
+        fprintf(stderr, "bcf_hdr_write error\n");
+        exit(1);
+    }
 
     bcf1_t* out_vcf_rec = bcf_init();
+    bcf_clear(out_vcf_rec);
 
-    // TODO: sort the output!
+    // TODO: sort the output?
     for (const auto& vs: contig2vars) { // vs: {seq, map}
         for (const auto& v: vs.second)  { // v: {pos, Varlist}
             auto it = v.second.begin();
@@ -138,40 +152,44 @@ void varcount(const VcntArgs& args) {
                 out_vcf_rec->rlen = it->ref.size();
                 bcf_update_alleles_str(out_vcf_hdr, out_vcf_rec, allele_str.data());
                 bcf_update_id(out_vcf_hdr, out_vcf_rec, it->id.data());
-                bcf_update_info_int32(out_vcf_hdr, out_vcf_rec, "ALTCNT", &(it->ac), 1);
-                bcf_update_info_int32(out_vcf_hdr, out_vcf_rec, "REFCNT", &(it->rc), 1);
+                bcf_update_info_int32(out_vcf_hdr, out_vcf_rec, "ALTCNT", &(it->ad[1]), 1);
+                bcf_update_info_int32(out_vcf_hdr, out_vcf_rec, "REFCNT", &(it->ad[0]), 1);
+                if (args.gl) {
+                    /* TODO: handle indels smarter. Use individual base qualities */
+                    auto pls = hts_util::get_pls_naive(it->ad[0], it->ad[1], ERROR_RATE);
+                    bcf_update_format_int32(out_vcf_hdr, out_vcf_rec, "PL", pls.data(), 3);
+                }
                 int32_t gt = 0;
                 if (args.gt) {
-                    if (args.thres < 0 && it->ac) {
-                        gt = bcf_gt_phased(it->ac != 0);
-                    } else if (!it->ac && !it->rc) {
-                        gt = bcf_gt_missing;
-                    } else if (std::abs(it->rc - it->ac) >= args.thres) {
-                        if (it->rc < it->ac) {
+                    gt = bcf_gt_missing;
+                    if (args.thres < 0 && it->ad[1]) {
+                        gt = bcf_gt_phased(it->ad[1] != 0);
+                    } else if ((it->ad[1] || it->ad[0]) && std::abs(it->ad[0] - it->ad[1]) >= args.thres) {
+                        if (it->ad[0] < it->ad[1]) {
                             gt = bcf_gt_phased(1);
-                        } else if (it->rc > it->ac)  {
+                        } else if (it->ad[0] > it->ad[1])  {
                             gt = bcf_gt_phased(0);
-                        } else { // it->rc == it->ac
+                        } else { // it->ad[0] == it->ad[1]
                             gt = bcf_gt_phased(args.alt_default);
                         }
-                    } else if (args.keep) {
-                        gt = bcf_gt_missing;
                     } 
-                } else {
-                    gt = bcf_gt_missing;
+                    if (args.diploid) { /* todo: predict an actual diploid */
+                        int32_t gts[2];
+                        gts[0] = gt;
+                        gts[1] = gt; // TODO: actually predict a second genotype.
+                        bcf_update_genotypes(out_vcf_hdr, out_vcf_rec, gts, 2);
+                    } else {
+                        int32_t gts[1];
+                        gts[0] = gt;
+                        bcf_update_genotypes(out_vcf_hdr, out_vcf_rec, gts, 1);
+                    }
                 }
-                if (args.diploid) {
-                    int32_t gts[2];
-                    gts[0] = gt;
-                    gts[1] = gt; // TODO: actually predict a second genotype.
-                    bcf_update_genotypes(out_vcf_hdr, out_vcf_rec, gts, 2);
-                } else {
-                    int32_t gts[1];
-                    gts[0] = gt;
-                    bcf_update_genotypes(out_vcf_hdr, out_vcf_rec, gts, 1);
-                }
-                if (args.keep || (it->ac >= args.min_ac && !bcf_gt_is_missing(gt))) {
-                    bcf_write(out_vcf_fp, out_vcf_hdr, out_vcf_rec);
+                if (args.keep || ( (it->ad[1] >= args.min_ac) && (!args.gt || (args.gt && !bcf_gt_is_missing(gt))) ) ) {
+                    bcf_update_format_int32(out_vcf_hdr, out_vcf_rec, "AD", it->ad.data(), 2);
+                    if (bcf_write(out_vcf_fp, out_vcf_hdr, out_vcf_rec)) {
+                        fprintf(stderr, "bcf_write error\n");
+                        exit(1);
+                    }
                 }
                 bcf_clear(out_vcf_rec);
 
@@ -228,7 +246,7 @@ int main(int argc, char** argv) {
 
     int ch;
     int argpos = 0;
-    while ( (ch = getopt_long(argc, argv, "-:s:c:m:dgvkh", long_options, NULL)) != -1 ) {
+    while ( (ch = getopt_long(argc, argv, "-:s:c:m:dgvkhl", long_options, NULL)) != -1 ) {
         switch(ch) {
             case 0:
                 break;
@@ -260,6 +278,9 @@ int main(int argc, char** argv) {
                 break;
             case 'v':
                 args.verbose = 1;
+                break;
+            case 'l':
+                args.gl = 1;
                 break;
             case 'h':
                 print_help();
